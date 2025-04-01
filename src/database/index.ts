3b39fcce7be0de476f6276c45f29e012fe4c7914
mayankrsagar <mayankrsagar@gmail.com>
2025-04-01 11:37:28 +0530

    update mongodb database to node

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,20 @@
 import mongoose from 'mongoose';
 
-let isConnected: boolean = false;
-
 const mongoURI = process.env.MONGODB_URI;
 
-
 export const connectToDB = async (): Promise<void> => {
-  if (isConnected) {
-    console.log("MongoDB is already connected.");
-    return;
-  }
-
   if (!mongoURI) {
     throw new Error("MongoDB URI is not defined.");
   }
 
+  // Prevent multiple connections
+  if (mongoose.connection.readyState >= 1) {
+    console.log("MongoDB is already connected.");
+    return;
+  }
+
   try {
-    await mongoose.connect(mongoURI);
-    isConnected = true;
+    await mongoose.connect(mongoURI, { dbName: "blogdb" });
     console.log("MongoDB connected successfully.");
   } catch (error) {
     console.error("MongoDB connection error:", error);
